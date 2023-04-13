2b500c7ea0c7cea9a8dac85d36652704145ae884
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-13 17:14:06 +0000

    return cached blogs, and updating when there is no

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -20,10 +20,15 @@ module.exports = app => {
     const client = redis.createClient(redisUrl);
     client.get = util.promisify(client.get);
     const cachedBlogs = await client.get(req.user.id);
+    if(cachedBlogs) {
+      return res.send(JSON.parse(cachedBlogs));
+    };
 
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs))
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
