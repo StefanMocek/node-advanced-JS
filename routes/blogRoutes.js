43f5b4867d24a4956319aa954a99ca7cc65443db
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-15 20:06:15 +0000

    restoring route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -17,18 +17,22 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const client = redis.createClient(redisUrl);
-    client.get = util.promisify(client.get);
-    const cachedBlogs = await client.get(req.user.id);
-    if(cachedBlogs) {
-      return res.send(JSON.parse(cachedBlogs));
-    };
+    // const client = redis.createClient(redisUrl);
+    // client.get = util.promisify(client.get);
+    // const cachedBlogs = await client.get(req.user.id);
+    // if(cachedBlogs) {
+    //   return res.send(JSON.parse(cachedBlogs));
+    // };
 
-    const blogs = await Blog.find({ _user: req.user.id });
+    // const blogs = await Blog.find({ _user: req.user.id });
 
-    res.send(blogs);
+    // res.send(blogs);
 
-    client.set(req.user.id, JSON.stringify(blogs))
+    // client.set(req.user.id, JSON.stringify(blogs))
+
+    const blogs = await Blog.find({_user: req.user.id});
+
+    res.send(blogs)
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
