05a67fcf8f627ca5c1f7cafa870ad6c1bf5275af
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-16 10:39:05 +0000

    unused code removed

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
 const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
-const redisUrl = 'redis://127.0.0.1:6379'
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -17,20 +14,7 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    // const client = redis.createClient(redisUrl);
-    // client.get = util.promisify(client.get);
-    // const cachedBlogs = await client.get(req.user.id);
-    // if(cachedBlogs) {
-    //   return res.send(JSON.parse(cachedBlogs));
-    // };
-
-    // const blogs = await Blog.find({ _user: req.user.id });
-
-    // res.send(blogs);
-
-    // client.set(req.user.id, JSON.stringify(blogs))
-
-    const blogs = await Blog.find({_user: req.user.id});
+ const blogs = await Blog.find({_user: req.user.id});
 
     res.send(blogs)
   });
