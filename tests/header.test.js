f80f0a7dd992d25bfd700fbbc4cd17bceed879bd
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-17 17:19:04 +0000

    creating page beforeEach and closing afterEach

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,12 +1,20 @@
 const puppeteer = require('puppeteer');
 
-test('Launching a browser', async () => {
-    const browser = await puppeteer.launch({
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
         headless: false
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto('localhost:3000');
+});
 
+afterEach(async () => {
+    await browser.close();
+});
+
+test('Launching a browser', async () => {
     const text = await page.$eval('a.brand-logo', () => el => el.innnerHTML);
 
     expect(text).toEqual('Blogster')
