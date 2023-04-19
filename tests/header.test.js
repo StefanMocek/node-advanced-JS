13c9c47bb1ed79467784495bae2bd64c139bc883
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-19 16:51:38 +0000

    getContentsOf mthod replacing $eval func

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,15 +11,14 @@ afterEach(async () => {
     await page.close();
 });
 
-test('the headerhas the correct text', async () => {
-    const text = await page.$eval('a.brand-logo', el => el.innnerHTML);
+test('the header has the correct text', async () => {
+    const text = await page.getContentsOf('a.brand-logo');
 
-    expect(text).toEqual('Blogster')
+    expect(text).toEqual('Blogster');
 });
 
 test('clicking login starts oauth flow', async () => {
     await page.click('.right a');
-
     const url = await page.url();
 
     expect(url).toMatch(/accounts\.google\.com/)
@@ -27,6 +26,7 @@ test('clicking login starts oauth flow', async () => {
 
 test('when sign in, shows logout button', async () => {
     await page.login();
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innnerHTML);
-    expect(text).toEqual('Logout')
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
+
+    expect(text).toEqual('Logout');
 })
