2153bca9e253ddd5546cc2a840ddcd6058ea5621
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-26 12:35:31 +0000

    next try

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -2,91 +2,88 @@ const Page = require('./helpers/page');
 
 let page;
 
-const myTitle = 'My test title';
-const myConent = 'My test content';
-
 beforeEach(async () => {
-    page = await Page.build();
-    await page.goto('http://localhost:3000');
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
-    await page.close();
+  await page.close();
 });
 
 describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test('can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using valid inputs', async () => {
     beforeEach(async () => {
-        await page.login();
-        await page.click('a.btn-floating');
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
     });
 
-    test('Can see blog create form', async () => {
-        const label = await page.getContentsOf('form label');
+    test('Submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
 
-        expect(label).toEqual('Blog title')
+      expect(text).toEqual('Please confirm your entries');
     });
 
-    describe('Using valid input', async () => {
-        beforeEach(async () => {
-            await page.type('.title input', myTitle);
-            await page.type('.content input', myConent);
-            await page.click('form button')
-        })
+    test('Submitting then saving adds blog to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
 
-        test('submiting takes user to review screen', async () => {
-            const text = await page.getContentsOf('h5');
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
 
-            expect(text).toEqual('Please confirm your entries');
-        });
-
-        test('submiting then saving adds blogs to index page', async () => {
-            await page.click('button.green');
-            await page.waitFor('.card');
-
-            const title = await page.getContentsOf('.card-title');
-            const content = await page.getContentsOf('p');
-
-            expect(title).toEqual(myTitle);
-            expect(content).toEqual(myConent);
-        })
-    })
-
-    describe('Using invalid inputs', async () => {
-        beforeEach(async () => {
-            await page.click('form button')
-        })
-
-        test('the form shows an error message', async () => {
-            const titleError = await page.getContentsOf('.title .red-text');
-            const contentError = await page.getContentsOf('.content .red-text');
-
-            expect(titleError).toEqual('You must provide a value');
-            expect(contentError).toEqual('You must provide a value');
-        })
-    })
-})
-
-describe('When user is not logged in', async () => {
-    const actions = [
-        {
-            method: 'get',
-            path: '/api/blogs'
-        },
-        {
-            method: 'post',
-            path: '/api/blogs',
-            data: { 
-                title: myTitle, 
-                content: myConent 
-            }
-        }
-    ];
-
-    test('Prohibited actions', async () => {
-        const resultArr = await page.execRequests(actions);
-
-        for (let result of resultArr) {
-            expect(result).toEqual({ error: 'You must log in!' })
-        };
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
     });
-})
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+});
+
+describe('User is not logged in', async () => {
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs'
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'T',
+        content: 'C'
+      }
+    }
+  ];
+
+  test('Blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!' });
+    }
+  });
+});
